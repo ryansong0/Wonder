f7566fc894c51f24e1802284abff64da8e33ca66
Ryan Song <ryansong129@gmail.com>
2026-06-01 21:21:41 -0400

    implemented DataIngestor to validate and process college data

--- src/schemas.py
+++ src/schemas.py
@@ -1,11 +1,15 @@
+#pydantic - industry standard for Data Validation
 from pydantic import BaseModel, Field
 from typing import Optional
 
+#blueprint for what the student is
 class StudentProfile(BaseModel):
     #The data a user enters once.
+    #... means a value must be provided, which is greater than 0
     household_income: float = Field(..., gt = 0)
-    total_assets: float = Field(..., ge = 0)
+    total_assets: float = Field(..., ge = 0) #ge=0 (greater than or equal to 0)
     family_size: int = Field(..., gt = 0)
+    #gt=0 (greater than 0) ensures the code rejects any nonsensical data (like a negative income)
     state_of_residence: str
 
 class CollegeData(BaseModel):
