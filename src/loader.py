9d6b6bcba3c9a83599a725cf85d5a56542cb7021
Ryan Song <ryansong129@gmail.com>
2026-06-03 19:10:40 -0400

    added csv loader module for college data

---
+++ src/loader.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from src.schemas import CollegeData
+
+def load_college_data(file_path: str) -> list[CollegeData]:
+    # read the csv file into a pandas DataFrame
+    df = pd.read_csv(file_path)
+    colleges = []
+
+    # iterate through each row in csv file
+    for _, row in df.iterrows():
+        # extract raw value
+        raw_value = row.get('endowment_size')
+        # convert to float if data exists
+        if raw_value:
+            endowment_size = float(raw_value)
+        else:
+            endowment_size = None
+        # create a CollegeData object for every row
+        college = CollegeData(
+            college_name = row['college_name'].
+            cost_of_attendance = float(row['cost_of_attendance']),
+        )
+        colleges.append(college)
+    return colleges
