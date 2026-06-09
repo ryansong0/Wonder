447902225d136d5e03ba3009a90f6bc7361a5120
Ryan Song <ryansong129@gmail.com>
2026-06-09 18:51:41 -0400

    Implemented college selection and created a student profile object

--- src/app.py
+++ src/app.py
@@ -9,10 +9,21 @@ st.set_page_config(page_title = "Financial Aid Simulation", layout = "wide")
 st.title("College Financial Aid Simulator")
 
 colleges = load_college_data('colleges.csv')
-colleges_names = [c.college_name for c in colleges]
+college_names = [c.college_name for c in colleges]
 
 st.sidebar.header("Student Profile")
 income = st.sidebar.number_input("Household Income ($)")
 assets = st.sidebar.number_input("Total Assets ($)")
 family_size = st.sidebar.slider("Family Size")
 state = st.sidebar.text_input("State", "NC")
+
+chosen_college_name = st.selectbox("Select Institution", college_names)
+chosen_college = next(c for c in colleges if c.college_name == chosen_college_name)
+
+if st.button("Run Simulation"):
+    student = StudentProfile(
+        household_income = income,
+        total_assets = assets,
+        family_size = family_size,
+        state_of_residence = state
+    )
